79660c62aad5e5e7e541a8ededab9e2c776724ef
Alissa <alissa.paluska@gmail.com>
2016-05-16 11:54:07 -0700

    add resource menu search_menu

--- app/src/main/java/epicodus/booktracker/Constants.java
+++ app/src/main/java/epicodus/booktracker/Constants.java
@@ -4,6 +4,8 @@ package epicodus.booktracker;
  * Created by ali on 5/16/16.
  */
 public class Constants {
+    public static final String PREFERENCES_SEARCHPARAM_KEY = "searchParam";
+
     public static final String GOOGLE_API_KEY = BuildConfig.GOOGLE_BOOK_API_KEY;
     public static final String GOOGLE_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
     public static final String GOOGLE_QUERY_PARAMETER = "searchParam";
--- app/src/main/java/epicodus/booktracker/ui/BookListActivity.java
+++ app/src/main/java/epicodus/booktracker/ui/BookListActivity.java
@@ -1,10 +1,17 @@
 package epicodus.booktracker.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.firebase.client.Firebase;
 
@@ -28,6 +35,9 @@ public class BookListActivity extends AppCompatActivity {
     private BookListAdapter mAdapter;
     //private Firebase mFirebaseRef;
 
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +51,44 @@ public class BookListActivity extends AppCompatActivity {
         String searchParam = intent.getStringExtra("searchParam");
         getBook(searchParam);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getBook(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addToSharedPreferences(String searchParam) {
+        mEditor.putString(Constants.PREFERENCES_SEARCHPARAM_KEY, searchParam).apply();
+    }
+
 
     private void getBook(String searchParam) {
         final GoogleBookService googleBookService = new GoogleBookService();
