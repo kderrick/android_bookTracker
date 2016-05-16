f2cc4616724580f017ba203f42c9109ea95053b7
Kyle <kderrick1988@gmail.com>
2016-05-16 11:16:43 -0700

    add CreateAccountACtivity layout

---
+++ app/src/main/java/epicodus/booktracker/CreateAccountActivity.java
@@ -0,0 +1,12 @@
+package epicodus.booktracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
