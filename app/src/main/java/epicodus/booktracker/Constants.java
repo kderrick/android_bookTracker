77a24802935720f63dec0a7e10cfe0385726acd9
chalmie <blairchalmers@gmail.com>
2016-05-16 13:17:18 -0700

    Update BookViewHolder

--- app/src/main/java/epicodus/booktracker/Constants.java
+++ app/src/main/java/epicodus/booktracker/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String GoogleBookApiKey = BuildConfig.GOOGLE_BOOK_API_KEY;
     public static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
     public static final String BOOK_QUERY_TITLE_PARAMETER = "q";
+
 }
