186bf5ba681df2bb423d301750842c22a6823439
Kyle <kderrick1988@gmail.com>
2016-05-17 09:50:40 -0700

    add functionality to save Book object to firebase on BookDetailFragment

--- app/src/main/java/epicodus/booktracker/ui/BookDetailFragment.java
+++ app/src/main/java/epicodus/booktracker/ui/BookDetailFragment.java
@@ -9,20 +9,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.Firebase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import epicodus.booktracker.Constants;
 import epicodus.booktracker.R;
 import epicodus.booktracker.model.Book;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BookDetailFragment extends Fragment {
+public class BookDetailFragment extends Fragment implements View.OnClickListener {
     @Bind(R.id.bookImageView) ImageView mImageLabel;
     @Bind(R.id.authorTextView) TextView mAuthorLabel;
     @Bind(R.id.bookNameTextView) TextView mBookNameLabel;
@@ -54,6 +57,7 @@ public class BookDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book_detail, container, false);
         ButterKnife.bind(this, view);
+        mSaveBookButton.setOnClickListener(this);
 
         Picasso.with(view.getContext()).load(mBook.getImage()).into(mImageLabel);
         mAuthorLabel.setText(mBook.getAuthor());
@@ -62,6 +66,15 @@ public class BookDetailFragment extends Fragment {
 //        mPageCountLabel.setText(mBook.getPageCount());
 
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mSaveBookButton) {
+            Firebase ref = new Firebase(Constants.FIREBASE_URL_BOOKS);
+            ref.push().setValue(mBook);
+            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
